50d7cd4c2a45de7ed734cac07d614ee2ed287683
unknown <hoangduccuongqb@gmail.com>
2017-09-17 17:01:32 +0700

    9/17/2017

---
+++ app/src/main/java/com/example/fullstack/appfoody/view/HomePageActivity.java
@@ -0,0 +1,19 @@
+package com.example.fullstack.appfoody.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.fullstack.appfoody.R;
+
+/**
+ * Created by FullStack on 9/16/2017.
+ */
+
+public class HomePageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_home_page);
+    }
+}
---
+++ app/src/main/java/com/example/fullstack/appfoody/view/SlashGreenActivity.java
@@ -0,0 +1,42 @@
+package com.example.fullstack.appfoody.view;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.fullstack.appfoody.R;
+
+public class SlashGreenActivity extends AppCompatActivity {
+    TextView txtVesion, txtFullName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_flashgreen);
+        txtVesion= (TextView) findViewById(R.id.txtVesion);
+        txtFullName = (TextView) findViewById(R.id.txtFullName);
+        txtFullName.setText(R.string.fullname);
+        try {
+            PackageInfo bPackageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+            txtVesion.setText(getString(R.string.vesion) +" " + bPackageInfo.versionName);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent =new Intent(SlashGreenActivity.this,HomePageActivity.class);
+                    startActivity(intent);
+                }
+            },2000);
+
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
